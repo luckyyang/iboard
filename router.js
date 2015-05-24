2bbd2130557df02c80b84db9c9999846b7cfe17b
Liu Jiyang <2411mail@gmail.com>
2015-05-24 14:24:05 +0800

    another method

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
+//meteor add iron:router
 Router.route('/', function () {
   this.render('homepage');
 });
-Router.route('/vote', function () {
+/*Router.route('/vote', function () {
   this.render('leaderboard');
-});
+});*/
+Router.route('/vote', {name: 'leaderboard'});
